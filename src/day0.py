0549b3477b9a2e1598dc37b3dbce0ba3924fd47c
Wojciech Sęk <wojciech.sek17@gmail.com>
2024-07-16 19:45:50 +0200

    testing pylint fixes

--- src/day0.py
+++ src/day0.py
@@ -1,6 +1,10 @@
+""" Day0 Module """
+
 def part1():
+    """Day0: Part1"""
     with open("data/day0.txt", "r", encoding="utf-8") as data:
         return data.read()
-    
+
 def part2():
+    """ Day0: Part2"""
     return 2
--- src/day0_test.py
+++ src/day0_test.py
@@ -1,7 +1,12 @@
+"""Test for day0.py"""
+
 import day0
 
 def test_part1():
+    """Test for part1"""
     assert day0.part1() == "Hello, World!"
 
 def test_part2():
+    """Test for part2"""
     assert day0.part2() == 2
+    
