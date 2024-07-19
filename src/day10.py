d2b159cd17dd55e7c50f82b7d978702948aab91c
Wojciech Sęk <wojciech.sek17@gmail.com>
2024-07-19 23:55:16 +0200

    day10

---
+++ src/day10.py
@@ -0,0 +1,60 @@
+def update_result(result, cycle, x, idxs):
+    cycle += 1
+    if cycle in idxs:
+        result += cycle * x
+    return cycle, result
+
+
+def part1():
+    with open("data/day10.txt", "r", encoding="utf-8") as data:
+        x = 1
+        cycle = 1
+        idxs = list(i for i in range(20, 221, 40))
+        result = 0
+
+        for line in data.readlines():
+            words = line.split()
+
+            if words[0] == "noop":
+                cycle, result = update_result(result, cycle, x, idxs)
+            else:
+                cycle, result = update_result(result, cycle, x, idxs)
+                x += int(words[1])
+                cycle, result = update_result(result, cycle, x, idxs)
+
+        return result
+
+
+def part2():
+    with open("data/day10.txt", "r", encoding="utf-8") as data:
+        x = 1
+        x_arr = [1]
+        cycle = 1
+        idxs = list(i for i in range(20, 221, 40))
+        result = 0
+
+        for line in data.readlines():
+            words = line.split()
+
+            if words[0] == "noop":
+                cycle += 1
+                x_arr.append(x)
+            else:
+                cycle += 1
+                x_arr.append(x)
+                x += int(words[1])
+                cycle += 1
+                x_arr.append(x)
+
+        rows = 6
+        cols = 40
+        result = ""
+
+        for row in range(rows):
+            for col in range(cols):
+                x = x_arr[row * cols + col]
+                c = "#" if abs(x - col) <= 1 else "."
+                result += c
+            result += "\n"
+
+        return result
---
+++ src/day10_test.py
@@ -0,0 +1,5 @@
+import day10
+
+
+def test_part1():
+    assert day10.part1() == 10760
