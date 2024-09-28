1a76f8994fe243a62ffa0b0c2b7938cefcc423e6
Wojciech Sęk <wojciech.sek17@gmail.com>
2024-09-28 14:33:55 +0200

    using bits for board representation

--- src/day17.py
+++ src/day17.py
@@ -1,69 +1,73 @@
 import itertools
 import dataclasses
 
+from operator import lshift, rshift
 from typing import Literal
 
 
 @dataclasses.dataclass
 class Shape:
-    cells: list[tuple[int, int]]
-    position: tuple[int, int]
+    cells: list[int]
+    position: int
 
-    def move_vertical(self):
-        self.position = (self.position[0] - 1, self.position[1])
+    def move_vertical(self) -> None:
+        self.position -= 1
 
-    def move_horizontal(self, jet: Literal["<", ">"]):
-        diff = 1 if jet == ">" else -1
-        self.position = (self.position[0], self.position[1] + diff)
+    def move_horizontal(self, jet: Literal["<", ">"]) -> None:
+        op = lshift if jet == "<" else rshift
+        self.cells = [op(c, 1) for c in self.cells]
 
-    def height(self):
-        return max(x for x, _ in self.cells) + self.position[0]
-
-    def absolute_cells(self) -> set[tuple[int, int]]:
-        return {(self.position[0] + x, self.position[1] + y) for x, y in self.cells}
+    def height(self) -> int:
+        return self.position + len(self.cells) - 1
 
 
-def get_minus(position: tuple[int, int]) -> Shape:
-    return Shape(cells=[(0, 0), (0, 1), (0, 2), (0, 3)], position=position)
+def get_minus(position: int) -> Shape:
+    return Shape(cells=[0b0011110], position=position)
 
 
-def get_plus(position: tuple[int, int]) -> Shape:
-    return Shape(cells=[(0, 1), (1, 0), (1, 1), (1, 2), (2, 1)], position=position)
+def get_plus(position: int) -> Shape:
+    return Shape(cells=[0b0001000, 0b0011100, 0b0001000], position=position)
 
 
-def get_lshape(position: tuple[int, int]) -> Shape:
-    return Shape(cells=[(0, 0), (0, 1), (0, 2), (1, 2), (2, 2)], position=position)
+def get_lshape(position: int) -> Shape:
+    return Shape(cells=[0b0011100, 0b0000100, 0b0000100], position=position)
 
 
-def get_ishape(position: tuple[int, int]) -> Shape:
-    return Shape(cells=[(0, 0), (1, 0), (2, 0), (3, 0)], position=position)
+def get_ishape(position: int) -> Shape:
+    return Shape(cells=[0b0010000, 0b0010000, 0b0010000, 0b0010000], position=position)
 
 
-def get_square(position: tuple[int, int]) -> Shape:
-    return Shape(cells=[(0, 0), (0, 1), (1, 0), (1, 1)], position=position)
+def get_square(position: int) -> Shape:
+    return Shape(cells=[0b0011000, 0b0011000], position=position)
 
 
 shape_getters = [get_minus, get_plus, get_lshape, get_ishape, get_square]
 
 
-def is_solid(board: set[tuple[int, int]], position: tuple[int, int]) -> bool:
-    x, y = position
-    return x <= 0 or y < 0 or y >= 7 or (x, y) in board
-
-
 def can_move_horizontal(
     board: set[tuple[int, int]], shape: Shape, jet: Literal["<", ">"]
 ) -> bool:
-    diff = 1 if jet == ">" else -1
-    abs_x, abs_y = shape.position
-    return all(
-        not is_solid(board, (abs_x + x, abs_y + y + diff)) for x, y in shape.cells
-    )
+    blocker = 0b1000000 if jet == "<" else 0b0000001
+
+    if any(cell & blocker for cell in shape.cells):
+        return False
+
+    op = lshift if jet == "<" else rshift
+    new_cells = [op(cell, 1) for cell in shape.cells]
+    pos = shape.position
+
+    return all(board[pos + i] & cell == 0 for i, cell in enumerate(new_cells))
 
 
-def can_move_vertical(board: set[tuple[int, int]], shape: Shape) -> bool:
-    abs_x, abs_y = shape.position
-    return all(not is_solid(board, (abs_x + x - 1, abs_y + y)) for x, y in shape.cells)
+def can_move_vertical(board: list[int], shape: Shape) -> bool:
+    pos = shape.position - 1
+    return all(board[pos + i] & cell == 0 for i, cell in enumerate(shape.cells))
+
+
+def get_new_board() -> list[int]:
+    board = 10_000 * [0b0000000]
+    board[0] = 0b1111111
+    return board
 
 
 def get_height(n: int):
@@ -71,11 +75,11 @@ def get_height(n: int):
         line = data.readline()
         jets = itertools.cycle(list(line.strip()))
         shape_getter = itertools.cycle(shape_getters)
-        board = set()
+        board = get_new_board()
         height = 0
 
         for _ in range(n):
-            shape = next(shape_getter)(position=(height + 4, 2))
+            shape = next(shape_getter)(position=height + 4)
             is_horizontal = True
             movable = True
 
@@ -93,7 +97,9 @@ def get_height(n: int):
                 is_horizontal = not is_horizontal
 
             height = max(height, shape.height())
-            board.update(shape.absolute_cells())
+
+            for i, cell in enumerate(shape.cells):
+                board[shape.position + i] |= cell
 
         return height
 
